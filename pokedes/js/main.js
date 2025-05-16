a9a80df8d8d3f4565d3c7666458cd002f407f6e9
endryus-montezano <endryusmontezano@live.com>
2025-05-16 16:51:11 -0300

    terminando a podedex

--- pokedes/js/main.js
+++ pokedes/js/main.js
@@ -1,28 +1,46 @@
 const pokemonlist = document.getElementById('pokemonlist')
+const verMais = document.getElementById('verMais')
+const maxRegistro = 649 
+const limit = 16
+let offset = 0
 
 
-
-function convertPokemonToHtml(pokemon) {
-    return `
+//arrow functions (funtion() igual a () =>)
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+    const newHtml = pokemons.map((pokemon) => `
             <li class="pokemon ${pokemon.type}">
                 <span class="numero">${pokemon.number}</span>
                 <span class="nome">${pokemon.name}</span>
+
                 <div class="detalhes">
                     <ol class="types">
                         ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('') }
                     </ol>
+
                     <img src="${pokemon.photo}" alt="${pokemon.name}">
                 </div>
             </li>
-    `
+        `).join('')
+
+    pokemonlist.innerHTML += newHtml
+    })
 }
 
-//arrow functions (funtion() igual a () =>)
-pokeApi.getPokemons().then((pokemons = []) => {
-    const newHtml = pokemons.map(convertPokemonToHtml).join('')
-    pokemonlist.innerHTML = newHtml
+loadPokemonItens(offset, limit)
+
+verMais.addEventListener('click', () => {
+    offset += limit
+
+    const quantidadeRegristro = offset + limit
+    if(quantidadeRegristro >= maxRegistro) {
+        const newLimit = maxRegistro - offset
+        loadPokemonItens(offset, newLimit)
+
+        verMais.parentElement.removeChild(verMais)
+    } else {
+         loadPokemonItens(offset, limit)
+    }
 })
-
-
 
 
